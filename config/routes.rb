3682197efb3aae9888eb9983a0e8df77cfefc3f3
Yarik Kovshovik <jaroslav.kovshovik@gmail.com>
2017-11-09 09:56:36 -0700

    minor improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   resources :photos
-  get 'challenges/index'
-  get 'challenges/save_checkbox'
 
   get 'welcome/index'
 
   resources :articles do
     resources :comments
   end
-  
   root 'welcome#index'
 end
